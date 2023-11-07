115cfc8640f7186982f257d086d196ac91148c67
Kristijan Bujak <cileb411@gmail.com>
2023-11-07 22:45:18 +0100

    IsSubsequence added

---
+++ IsSubsequence/IsSubsequence.js
@@ -0,0 +1,51 @@
+let s = "aaaaaa";
+let t = "bbaaaa";
+var isSubsequence = function(s, t) {
+    let isFound;
+    let indexFound=0;
+    if(s.length === 0){
+        return true
+    }
+    for(let i = 0; i< s.length; i++){
+        isFound = false;
+        for(let j=0; j<t.length; j++){
+            if(s[i] === t[j] && j>=indexFound){
+                isFound = true;
+                indexFound = j+1;
+                console.log("index found: " + indexFound + " , " + s[i] + " , " + t[j]);
+                break;
+            }
+        }
+        if(isFound === false){
+            return false;
+        }
+    }
+    if(isFound === false){
+        return false;
+    }
+    else{
+        return true;
+    }
+};
+
+// let s = "abc";
+// let t = "ahbgdc";
+
+console.log(isSubsequence(s, t));
+
+// bolje resenje
+// function isSubsequence(s, t) {
+//     let i = 0; // Pointer for s
+//     let j = 0; // Pointer for t
+  
+//     while (i < s.length && j < t.length) {
+//       if (s[i] === t[j]) {
+//         i++;
+//       }
+//       j++; 
+//     }
+//   
+//     return i === s.length;
+//   }
+
+
